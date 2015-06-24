6f740cdb43f770ff4bbaf1858ade8473244ee099
Kim Ortvald <keiko@keikoware.dk>
2015-06-24 11:33:06 +0200

    Db_creation script modyfied

--- setup/create_db.sql
+++ setup/create_db.sql
@@ -1,6 +1,5 @@
 -- phpMyAdmin SQL Dump
 
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -17,9 +16,9 @@ GRANT ALL PRIVILEGES ON osama_db.* TO osama WITH GRANT OPTION;
 
 USE `osama_db`;
 
-DROP TABLE IF EXISTS `people`;
-CREATE TABLE IF NOT EXISTS `people` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) COLLATE utf8_danish_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_danish_ci DEFAULT NULL,
   `firstname` varchar(250) COLLATE utf8_danish_ci DEFAULT NULL,
@@ -27,21 +26,27 @@ CREATE TABLE IF NOT EXISTS `people` (
   `phone` varchar(50) COLLATE utf8_danish_ci DEFAULT NULL,
   `mail` varchar(250) COLLATE utf8_danish_ci DEFAULT NULL,
   `lastLoginDatetime` datetime DEFAULT NULL,
+   -- standard revision fields
+  `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
+  `updatedBy` int(11) DEFAULT NULL,
   `updatedDatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `retired` tinyint(4) NOT NULL DEFAULT '0',
-  CONSTRAINT pk_people_idx PRIMARY KEY (id),
-  CONSTRAINT udx_people_username UNIQUE KEY (`username`)
+  CONSTRAINT pk_user_idx PRIMARY KEY (id),
+  CONSTRAINT udx_user_username UNIQUE KEY (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 DROP TABLE IF EXISTS `service`;
 CREATE TABLE IF NOT EXISTS `service` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `serviceName` varchar(250) COLLATE utf8_danish_ci NOT NULL,
   `serviceDescription` varchar(4000) COLLATE utf8_danish_ci DEFAULT NULL,
-  `serviceOwner` int(11) DEFAULT NULL,
-  `serviceCreatedDatetime` datetime DEFAULT NULL,
-  `serviceUpdatedDatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `owner` int(11) DEFAULT NULL,
+   -- standard revision fields
+  `createdBy` int(11) DEFAULT NULL,
+  `createdDatetime` datetime DEFAULT NULL,
+  `updatedBy` int(11) DEFAULT NULL,
+  `updatedDatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `retired` tinyint(4) NOT NULL DEFAULT '0',
   CONSTRAINT pk_service_idx PRIMARY KEY (id),
   CONSTRAINT udx_service_servicename UNIQUE KEY (`serviceName`)
@@ -49,19 +54,20 @@ CREATE TABLE IF NOT EXISTS `service` (
 
 DROP TABLE IF EXISTS `task`;
 CREATE TABLE IF NOT EXISTS `task` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `taskUID` varchar(250) COLLATE utf8_danish_ci NOT NULL,
   `taskName` varchar(250) COLLATE utf8_danish_ci NOT NULL,
   `taskDescription` varchar(40000) COLLATE utf8_danish_ci DEFAULT NULL, -- html description of the complete task
   `expectedDurationMinutes` int(11) NOT NULL, -- duration in minutes 
   `expectedDurationHours` int(11) NOT NULL, -- duration in hours
+  `service_id` int(11) NULL,
+  `parentTask_id` int(11) NULL,
+  `usergroup_id` int(11) NULL,
+   -- standard revision fields
   `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
   `updatedBy` int(11) DEFAULT NULL,
   `updatedDatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `service_id` int(11) NULL,
-  `parentTask_id` int(11) NULL,
-  `ressourcepool_id` int(11) NULL,
   `retired` tinyint(4) NOT NULL DEFAULT '0',
   CONSTRAINT pk_task_idx PRIMARY KEY (id),
   CONSTRAINT udx_task_taskName UNIQUE KEY (`taskUID`)
@@ -69,10 +75,11 @@ CREATE TABLE IF NOT EXISTS `task` (
 
 DROP TABLE IF EXISTS `service`;
 CREATE TABLE IF NOT EXISTS `service` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `serviceName` varchar(250) COLLATE utf8_danish_ci NOT NULL,
   `serviceDescription` varchar(4000) COLLATE utf8_danish_ci DEFAULT NULL,
   `serviceOwner` int(11) DEFAULT NULL,
+  -- standard revision fields
   `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
   `updatedBy` int(11) DEFAULT NULL,
@@ -84,10 +91,9 @@ CREATE TABLE IF NOT EXISTS `service` (
 
 DROP TABLE IF EXISTS `taskSchedule`;
 CREATE TABLE IF NOT EXISTS `taskSchedule` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_id` int(11) NOT NULL,
   `beginTask` varchar(250) COLLATE utf8_danish_ci DEFAULT NULL, -- 'On Schedule', 'At Startup', 'At Log In', 'On Idle'
-
   `schedule` varchar(250) COLLATE utf8_danish_ci DEFAULT NULL, -- 'Ad Hoc Only', 'Daily', 'Weekly', 'Monthly' -- must add ,'Quarterly', 'Once every half year', 'Once a year'
   `startDateTime` datetime DEFAULT NULL, 
   `recurEvery` int(11) DEFAULT NULL, -- daily = day interval, weeks = week interval
@@ -95,7 +101,6 @@ CREATE TABLE IF NOT EXISTS `taskSchedule` (
   `recurMonths` varchar(500) DEFAULT NULL, --  multiple select months: 1,2,3,1 .. 11,12
   `recurMonthDays` varchar(500) DEFAULT NULL, -- multiple select dates: 1,2,3,4 .. 30,31 
   `recurMonthOndays` varchar(500) DEFAULT NULL, -- First, Second, Third, Fourth, Last - in addition to weekdays
-
   -- advanced setting only used to automate task with delay and repetition
   `delayTask` tinyint(4) NOT NULL DEFAULT '0',
   `delayTaskFor` int(11) DEFAULT NULL, -- integer representation of number of duration that the task should be delayed after triggered
@@ -103,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `taskSchedule` (
   `repeatTask` tinyint(4) NOT NULL DEFAULT '0',
   `repeatTaskFor` int(11) DEFAULT NULL, -- integer representation of number of duration that the task should be delayed after triggered
   `repeatTaskDuration` int(11) DEFAULT NULL, -- minutes, hours, days
-
+  -- standard revision fields
   `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
   `updatedBy` int(11) DEFAULT NULL,
@@ -112,13 +117,13 @@ CREATE TABLE IF NOT EXISTS `taskSchedule` (
   CONSTRAINT pk_taskschedule_idx PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
-
 DROP TABLE IF EXISTS `scheduledTask`;
 CREATE TABLE IF NOT EXISTS `scheduledTask` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `task_id` int(11) NOT NULL,
   `startDateTime` datetime DEFAULT NULL, 
   `expireDateTime` datetime DEFAULT NULL, 
+  -- standard revision fields
   `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
   `updatedBy` int(11) DEFAULT NULL,
@@ -127,69 +132,79 @@ CREATE TABLE IF NOT EXISTS `scheduledTask` (
   CONSTRAINT pk_scheduledtask_idx PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
-DROP TABLE IF EXISTS `ressourcePool`;
-CREATE TABLE IF NOT EXISTS `ressourcePool` (
-  `id` int(11) NOT NULL,
-  `ressourcePoolName` varchar(250) COLLATE utf8_danish_ci NOT NULL,
-  `ressourcePoolDescription` varchar(4000) COLLATE utf8_danish_ci DEFAULT NULL,
-  `ressourcePoolOwner` int(11) DEFAULT NULL,
+DROP TABLE IF EXISTS `usergroup`;
+CREATE TABLE IF NOT EXISTS `usergroup` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usergroupName` varchar(250) COLLATE utf8_danish_ci NOT NULL,
+  `usergroupDescription` varchar(4000) COLLATE utf8_danish_ci DEFAULT NULL,
+  `usergroupOwner` int(11) DEFAULT NULL,
+   -- standard revision fields
   `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
   `updatedBy` int(11) DEFAULT NULL,
   `updatedDatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `retired` tinyint(4) NOT NULL DEFAULT '0',
-  CONSTRAINT pk_ressourcepool_idx PRIMARY KEY (id)
+  CONSTRAINT pk_usergroup_idx PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
-DROP TABLE IF EXISTS `humanPooling`;
-CREATE TABLE IF NOT EXISTS `humanPooling` (
-  `ressourcePool_id` int(11) NOT NULL,
-  `humanRessource_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `membership`;
+CREATE TABLE IF NOT EXISTS `membership` (
+  `usergroup_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `note` varchar(4000) COLLATE utf8_danish_ci DEFAULT NULL,
+   -- standard revision fields
   `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
   `exppireDatetime` datetime DEFAULT NULL, -- if the pooling depends on a certification that runs out, this date is set.
   `updatedBy` int(11) DEFAULT NULL,
   `updatedDatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `retired` tinyint(4) NOT NULL DEFAULT '0',
-  CONSTRAINT pk_humanPooling_idx PRIMARY KEY (`ressourcePool_id`,`humanRessource_id`)
+  CONSTRAINT pk_membership_idx PRIMARY KEY (`usergroup_id`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
-DROP TABLE IF EXISTS `servicePooling`;
-CREATE TABLE IF NOT EXISTS `servicePooling` (
-  `ressourcePool_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `serviceusergroup`;
+CREATE TABLE IF NOT EXISTS `serviceusergroup` (
+  `usergroup_id` int(11) NOT NULL,
   `service_id` int(11) NOT NULL,
   `note` varchar(4000) COLLATE utf8_danish_ci DEFAULT NULL,
+   -- standard revision fields
   `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
   `exppireDatetime` datetime DEFAULT NULL, -- if the pooling depends on a certification that runs out, this date is set.
   `updatedBy` int(11) DEFAULT NULL,
   `updatedDatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `retired` tinyint(4) NOT NULL DEFAULT '0',
-  CONSTRAINT pk_servicePooling_idx PRIMARY KEY (`ressourcePool_id`,`service_id`)
+  CONSTRAINT pk_serviceusergroup_idx PRIMARY KEY (`usergroup_id`,`service_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 
-DROP TABLE IF EXISTS `feedback`;
-CREATE TABLE IF NOT EXISTS `feedback` (
-  `id` int(11) NOT NULL,
-  `humanRessource_id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `taskfeedback`;
+CREATE TABLE IF NOT EXISTS `taskfeedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `task_id` int(11) NOT NULL,
   `status` varchar(40) COLLATE utf8_danish_ci DEFAULT NULL,  -- completed, cancelled, expired, rescheduled,
   `note` varchar(4000) COLLATE utf8_danish_ci DEFAULT NULL,
   `durationMinutes` int(11) NOT NULL, -- duration in minutes 
   `durationHours` int(11) NOT NULL, -- duration in hours
+   -- standard revision fields
+  `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
-  CONSTRAINT pk_feedback_idx PRIMARY KEY (`id`)
+  `exppireDatetime` datetime DEFAULT NULL, -- if the pooling depends on a certification that runs out, this date is set.
+  `updatedBy` int(11) DEFAULT NULL,
+  `updatedDatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `retired` tinyint(4) NOT NULL DEFAULT '0',
+  CONSTRAINT pk_taskfeedback_idx PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE IF NOT EXISTS `role` (
-  `id` int(11) NOT NULL,
-  `humanRessource_id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
   `role` varchar(40) COLLATE utf8_danish_ci DEFAULT NULL,  -- completed, cancelled, expired, rescheduled,
   `note` varchar(4000) COLLATE utf8_danish_ci DEFAULT NULL,
+   -- standard revision fields
   `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
   `updatedBy` int(11) DEFAULT NULL,
@@ -198,18 +213,19 @@ CREATE TABLE IF NOT EXISTS `role` (
   CONSTRAINT pk_role_idx PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
-DROP TABLE IF EXISTS `humanRole`;
-CREATE TABLE IF NOT EXISTS `humanRole` (
+DROP TABLE IF EXISTS `userRole`;
+CREATE TABLE IF NOT EXISTS `userRole` (
   `role_id` int(11) NOT NULL,
-  `humanRessource_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `note` varchar(4000) COLLATE utf8_danish_ci DEFAULT NULL,
+   -- standard revision fields
   `createdBy` int(11) DEFAULT NULL,
   `createdDatetime` datetime DEFAULT NULL,
   `exppireDatetime` datetime DEFAULT NULL, -- if the role depends on a permit that runs out, this date is set.
   `updatedBy` int(11) DEFAULT NULL,
   `updatedDatetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `retired` tinyint(4) NOT NULL DEFAULT '0',
-  CONSTRAINT pk_humanRole_idx PRIMARY KEY (`role_id`,`humanRessource_id`)
+  CONSTRAINT pk_humanRole_idx PRIMARY KEY (`role_id`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
